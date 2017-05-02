57ebfb7cb0bdad8100aea3da0a9707103ddfb61e
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-02 07:20:01 +0700

    Ini dicopy aja gitu dari souurce code

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,44 +1,35 @@
 // const MongoClient = require('mongodb').MongoClient;
-
 const {MongoClient, ObjectID} = require('mongodb');
 
-// var obj = new ObjectID();
-
-// console.log(obj);
-
-// var user = {name: 'Faza', age: 27};
-// var {age} = user;
-//
-// console.log(age);
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-  if(err){
-    console.log('Unable to connect to MongoDB server');
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
   }
-  console.log('connected to MongoDB Server');
+  console.log('Connected to MongoDB server');
 
   // db.collection('Todos').insertOne({
-  //   text: 'Something todo',
+  //   text: 'Something to do',
   //   completed: false
   // }, (err, result) => {
-  //   if(err){
+  //   if (err) {
   //     return console.log('Unable to insert todo', err);
   //   }
   //
   //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // })
+  // });
 
+  // Insert new doc into Users (name, age, location)
   // db.collection('Users').insertOne({
-  //   name: 'Faza Fahamsyah',
-  //   age: 27,
-  //   location: 'Tangerang Selatan'
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
   // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert todo', err);
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
   //   }
   //
   //   console.log(result.ops[0]._id.getTimestamp());
-  // })
+  // });
 
   db.close();
 });
